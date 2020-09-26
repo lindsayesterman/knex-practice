58c534900a3a2aede465268ba0e413bbc8e4be84
Lindsay Esterman <lindsayesterman@gmail.com>
2020-09-26 08:36:00 -0400

    drill #4

--- src/drills.js
+++ src/drills.js
@@ -3,52 +3,67 @@ const knex = require('knex')
 
 
 const knexInstance = knex({
-    client: 'pg',
-    connection: process.env.DB_URL
+  client: 'pg',
+  connection: process.env.DB_URL
 })
 
 console.log('knex and driver installed correctly');
 
 const searchTerm = 'Fish Tricks'
 
-knexInstance
-    .select('name')
-    .from('shopping_list')
-    .where('name', 'ILIKE',  `%${searchTerm}%`)
-    .first()
-    .then(result => {
-        console.log(result)
-    })
+  knexInstance
+  .select('name')
+  .from('shopping_list')
+  .where('name', 'ILIKE',  `%${searchTerm}%`)
+  .first()
+  .then(result => {
+    console.log(result)
+  })
 
 
 function paginateProducts(page) {
-    const productsPerPage = 10
-    const offset = productsPerPage * (page - 1)
-    knexInstance
-    .select('price')
-    .from('shopping_list')
-    .limit(productsPerPage)
-    .offset(offset)
-    .then(result => {
-        console.log(result)
-      })
-  }
-  
-  paginateProducts(2)
-
-  function itemsAfterDay(days) {
+  const productsPerPage = 10
+  const offset = productsPerPage * (page - 1)
   knexInstance
-    .select('name')
-    .count('date_added AS views')
+  .select('*')
+  .from('shopping_list')
+  .limit(productsPerPage)
+  .offset(offset)
+  .then(result => {
+    console.log(result)
+  })
+}
+
+paginateProducts(2)
+
+
+function productsAddedDaysAgo(daysAgo) {
+  knexInstance
+    .select('id', 'name', 'price', 'date_added', 'checked', 'category')
+    .from('shopping_list')
     .where(
       'date_added',
       '>',
-      knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+      knexInstance.raw(`now() - '?? days':: INTERVAL`, daysAgo)
     )
-    .from('shopping_list')
-    .groupBy('name')
-    .then(result => {
-      console.log(result)
+    .then(results => {
+      console.log('PRODUCTS ADDED DAYS AGO')
+      console.log(results)
     })
 }
-    itemsAfterDay(30)
+
+productsAddedDaysAgo(5)
+
+
+function costPerCategory() {
+  knexInstance
+      .select('category')
+      .from('shopping_list')
+      .groupBy('category')
+      .count('price AS total')
+      .then(result => {
+          console.log(result)
+      })
+}
+
+  costPerCategory()
